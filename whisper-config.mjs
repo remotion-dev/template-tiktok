b2fa82e8f48ac914fd0482765341ddade22a2b50
Jonny Burger <jonathanburger11@gmail.com>
2024-02-22 12:14:07 +0100

    new sample video

--- whisper-config.mjs
+++ whisper-config.mjs
@@ -24,4 +24,4 @@ export const WHISPER_VERSION = '1.5.4';
 /**
  * @type {import('@remotion/install-whisper-cpp').WhisperModel}
  */
-export const WHISPER_MODEL = 'medium';
+export const WHISPER_MODEL = 'medium.en';
