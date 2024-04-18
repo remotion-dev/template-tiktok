216a7aeb6fdf0e957de3f3743775067078b82437
Jonny Burger <jonathanburger11@gmail.com>
2024-04-18 09:51:49 +0200

    Update whisper-config.mjs

--- whisper-config.mjs
+++ whisper-config.mjs
@@ -4,7 +4,7 @@ import path from 'node:path';
 export const WHISPER_PATH = path.join(process.cwd(), 'whisper.cpp');
 
 // The version of Whisper.cpp to install
-export const WHISPER_VERSION = '48a145';
+export const WHISPER_VERSION = '1.5.5';
 
 // Which model to use.
 // | Model      | Disk   | Mem      |
